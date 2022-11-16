a97447bfabd1bd5b9573e60c408962698a96af6f
Jeonghoon <hxxjxxxhxxx@gmail.com>
2022-11-16 22:54:03 +0900

    실제멤버추가 DB연동페이지까지 교통정리

--- app.py
+++ app.py
@@ -2,23 +2,18 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-# client = MongoClient('mongodb+srv://test:test@cluster0.nkgrata.mongodb.net/cluster0?retryWrites=true&w=majority')
-# db = client.test
-
 client = MongoClient('mongodb+srv://test:sparta@cluster0.qsyxhqm.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
-<<<<<<< HEAD
 
-=======
-#
-@app.route('/members')
+
+@app.route('/members-layout')
 def getmembers():
     return render_template('members_layout.html')
->>>>>>> fcce1dfc804a009cad7b652896a2357f9001cb17
+
 
 @app.route('/members_up')
 def members_up():
@@ -52,20 +47,20 @@ def members_db():
     db.members.insert_one(doc)
     return jsonify({'msg': '조원추가 완료!'})
 #
-# @app.route("/join", methods=["POST"])
-# def join_post():
-#     name_receive = request.form['name_give']
-#     doc = {
-#         'enName' :name_receive
-#     }
-#     db.join.insert_one(doc)
-#
-#     return jsonify({'msg':'합류 신청 완료!'})
-#
-# @app.route("/join", methods=["GET"])
-# def join_get():
-#     first_name = list(db.join.find({}, {'_id': False}))
-#     return jsonify({'firstName':first_name})
+@app.route("/join", methods=["POST"])
+def join_post():
+    name_receive = request.form['name_give']
+    doc = {
+        'enName' :name_receive
+    }
+    db.join.insert_one(doc)
+
+    return jsonify({'msg':'합류 신청 완료!'})
+
+@app.route("/join", methods=["GET"])
+def join_get():
+    first_name = list(db.join.find({}, {'_id': False}))
+    return jsonify({'firstName':first_name})
 
 @app.route("/guestBook", methods=["POST"])
 def guest_post():
@@ -86,9 +81,9 @@ def guest_get():
     guests_list = list(db.guests.find({}, {'_id': False}))
     return jsonify({'guests':guests_list})
 
-@app.route('/members-layout')
-def members_get():
-    return render_template('members_layout.html')
+# @app.route('/members-layout')
+# def members_get():
+#     return render_template('members_layout.html')
 
 # @app.route('/members-layout/:id')
 # def members_get():
