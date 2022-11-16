b4c6a8346e14bf89fa1a2ccb0d2252a56ce72930
Jeonghoon <hxxjxxxhxxx@gmail.com>
2022-11-16 11:04:24 +0900

    합류하기 닫기버튼 추가 및 UI일부 수정

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,12 @@
 function toggleBtn() {
     const joinWrap = document.getElementById("joinWrap");
+    const joinBtn = document.getElementById("joinBtn");
     if(joinWrap.style.display !=='none') {
         joinWrap.style.display = 'none';
+        joinBtn.style.display = 'block';
     } else {
         joinWrap.style.display = 'block';
+        joinBtn.style.display = 'none';
     }
 }
 
